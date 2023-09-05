682fb02295a80bd2677c56938c19620fa5d9766e
Deadabdul <a.oguntade11@gmail.com>
2023-09-05 14:47:29 -0600

    awmd

--- main.js
+++ main.js
@@ -1,49 +1,60 @@
-// Olympic Rings Start Code
+// Triangle Heart Asgn Solution
 
 // Canvas Setup
 let cnv = document.getElementById("my-canvas");
 let ctx = cnv.getContext("2d");
-cnv.width = 440;
-cnv.height = 250;
+cnv.width = 400;
+cnv.height = 400;
 
 // Call draw function once all page resources have loaded
 window.addEventListener("load", draw);
 
 function draw() {
-  // Background
-  ctx.fillStyle = "#eee";
-  ctx.fillRect(0, 0, cnv.width, cnv.height);
+  // Draw all GREY Triangles
+  ctx.fillStyle = "grey";
 
-  // Circle Line Width
-  ctx.lineWidth = 8;
-
-  // Blue Ring - Center: (100, 100) Radius: 50
-  ctx.strokeStyle = "blue";
   ctx.beginPath();
-  ctx.arc(100, 100, 50, 0, 2 * Math.PI);
-  ctx.stroke();
+  ctx.moveTo(0, 100);
+  ctx.lineTo(100, 0);
+  ctx.lineTo(100, 100);
+  ctx.fill();
 
-  // Yellow Ring - Center: (160, 150) Radius: 50
-  ctx.strokeStyle = "yellow";
   ctx.beginPath();
-  ctx.arc(160, 150, 50, 0, 2 * Math.PI);
-  ctx.stroke();
+  ctx.moveTo(200, 100);
+  ctx.lineTo(300, 0);
+  ctx.lineTo(300, 100);
+  ctx.fill();
 
-  // Black Ring - Center: (220, 100) Radius: 50
-  ctx.strokeStyle = "black";
   ctx.beginPath();
-  ctx.arc(220, 100, 50, 0, 2 * Math.PI);
-  ctx.stroke();
+  ctx.moveTo(100, 100);
+  ctx.lineTo(100, 200);
+  ctx.lineTo(200, 100);
+  ctx.fill();
 
-  // Blue Ring - Center: (280, 150) Radius: 50
-  ctx.strokeStyle = "green";
   ctx.beginPath();
-  ctx.arc(280, 150, 50, 0, 2 * Math.PI);
-  ctx.stroke();
+  ctx.moveTo(300, 100);
+  ctx.lineTo(300, 200);
+  ctx.lineTo(400, 200);
+  ctx.fill();
 
-  // Red Ring - Center: (340, 100) Radius: 50
-  ctx.strokeStyle = "red";
   ctx.beginPath();
-  ctx.arc(340, 100, 50, 0, 2 * Math.PI);
-  ctx.stroke();
+  ctx.moveTo(100, 200);
+  ctx.lineTo(100, 300);
+  ctx.lineTo(200, 200);
+  ctx.fill();
+
+  ctx.beginPath();
+  ctx.moveTo(200, 300);
+  ctx.lineTo(200, 400);
+  ctx.lineTo(300, 300);
+  ctx.fill();
+
+  // Draw all PINK Triangles
+  ctx.fillStyle = "pink";
+
+  // Draw all RED Triangles
+  ctx.fillStyle = "red";
+
+  // Draw all YELLOW Triangles
+  ctx.fillStyle = "yellow";
 }
